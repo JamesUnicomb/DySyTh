2925d18e9e2acf0b6c367dfa5d1dd1c4c3b938de
122445 <james.unicomb@uts.edu.au>
2018-11-30 23:19:24 +1100

    .

--- DoublePendulum.py
+++ DoublePendulum.py
@@ -63,14 +63,14 @@ def plot_path():
     m      = 1.0
     g      = 9.81
 
-    test_array = RK4(np.array([theta1, theta2, 0.0, 0.0]),
+    state_array = RK4(np.array([theta1, theta2, 0.0, 0.0]),
                          0.005, m, l, g, 4000)
 
-    x1 = l * np.cos(test_array[:,0] - np.pi / 2)
-    y1 = l * np.sin(test_array[:,0] - np.pi / 2)
+    x1 = l * np.cos(state_array[:,0] - np.pi / 2)
+    y1 = l * np.sin(state_array[:,0] - np.pi / 2)
 
-    x2 = x1 + l * np.cos(test_array[:,1] - np.pi / 2)
-    y2 = y1 + l * np.sin(test_array[:,1] - np.pi / 2)
+    x2 = x1 + l * np.cos(state_array[:,1] - np.pi / 2)
+    y2 = y1 + l * np.sin(state_array[:,1] - np.pi / 2)
 
 
 
@@ -90,7 +90,7 @@ def plot_path():
         label = 'timestep {0}'.format(k)
         print label
 
-        idt = int((1.0 * k) / 800 * len(test_array))
+        idt = int((1.0 * k) / 800 * len(state_array))
         pendulum_line.set_xdata([0.0, x1[idt], x2[idt]])
         pendulum_line.set_ydata([0.0, y1[idt], y2[idt]])
 
@@ -106,6 +106,40 @@ def plot_path():
 
 
 def plot_zero_crossing(K = 600):
+    x = T.ftensor3()
+
+    def f(X):
+        X_ = T.zeros_like(X)
+        X_ = T.set_subtensor(X_[:,:,0], (6.0 / (mass * length * length)) * \
+                                        ((2.0 * X[:,:,2] - 3.0 * T.cos(X[:,:,0] - X[:,:,1]) * X[:,:,3]) / \
+                                        (16.0 - 9.0 * T.square(T.cos(X[:,:,0] - X[:,:,1])))))
+        X_ = T.set_subtensor(X_[:,:,1], (6.0 / (mass * length * length)) * \
+                                        (8.0 * X[:,:,3] - 3.0 * T.cos(X[:,:,0] - X[:,:,1]) * X[:,:,2]) / \
+                                        (16.0 - 9.0 * T.square(T.cos(X[:,:,0] - X[:,:,1]))))
+        X_ = T.set_subtensor(X_[:,:,2], -0.5 * mass * length * length * (X_[:,:,0] * X_[:,:,1] * T.sin(X[:,:,0] - X[:,:,1]) + \
+                                                                     3.0 * gravity / length * T.sin(X[:,:,0])))
+        X_ = T.set_subtensor(X_[:,:,3], -0.5 * mass * length * length * (-X_[:,:,0] * X_[:,:,1] * T.sin(X[:,:,0] - X[:,:,1]) + \
+                                                                         gravity / length * T.sin(X[:,:,1])))
+        return X_
+
+    def step(X):
+        k1 = h * f(X)
+        k2 = h * f(X + 0.5 * k1)
+        k3 = h * f(X + 0.5 * k2)
+        k4 = h * f(X + k3)
+
+        X_ = X + (1.0 / 6.0) * k1 + (1.0 / 3.0) * k2 + (1.0 / 3.0) * k3 + (1.0 / 6.0) * k4
+
+        return X_
+
+    result, _ = theano.scan(fn=step,
+                            outputs_info=x,
+                            n_steps=N)
+
+    RK4 = theano.function([x,h,mass,length,gravity,N],
+                          result,
+                          allow_input_downcast=True)
+
     l      = 1.0
     m      = 1.0
     g      = 9.81
@@ -113,21 +147,21 @@ def plot_zero_crossing(K = 600):
     theta1_, theta2_ = np.meshgrid(np.linspace(-np.pi, np.pi, K),
                                    np.linspace(-np.pi, np.pi, K))
 
-    thetas = np.column_stack((theta1_.flatten(), theta2_.flatten()))
+    initial_states = np.stack((theta1_, theta2_, np.zeros_like(theta1_), np.zeros_like(theta2_)), axis=2)
+
+    state_array = RK4(initial_states, 0.025, m, l, g, 500)
+
     min_crossing_time = []
 
-    for theta1, theta2 in thetas:
-        print np.round(theta1,2), np.round(theta2,2), '\r',
-        state_array = RK4(np.array([theta1, theta2, 0.0, 0.0]),
-                             0.1, m, l, g, 300)
+    for i in range(state_array.shape[1]):
+        for j in range(state_array.shape[2]):
+            theta_diff    = np.mod(state_array[:,i,j,0] - state_array[:,i,j,1] - np.pi, 2.0 * np.pi)
+            crossings     = np.abs(np.diff(theta_diff)) > np.pi
 
-        theta_diff    = np.mod(state_array[:,0] - state_array[:,1] - np.pi, 2.0 * np.pi)
-        crossings     = np.abs(np.diff(theta_diff)) > np.pi
-
-        if np.sum(crossings) == 0:
-            min_crossing_time.append(np.nan)
-        else:
-            min_crossing_time.append(np.min(np.where(crossings)))
+            if np.sum(crossings) == 0:
+                min_crossing_time.append(np.nan)
+            else:
+                min_crossing_time.append(np.min(np.where(crossings)))
 
     min_crossing_time = np.array(min_crossing_time)
 
